d2784ca9bd81aad1546946b74df38830a8acd595
Nilesh Mishra <nilesh.mishra@globalids.com>
2022-11-03 16:21:40 +0530

    portfolio

---
+++ portfolio/src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container fluid grid-list-md style="background:#2b44ff;height: 100%;">
+    <v-row>
+        <v-col class="pa-0 ma-0" >
+            <Mobile/>
+        </v-col>
+        <v-col class="pa-0 ma-0">
+            <vue-terminal :task-list="taskList" :command-list="commandList" />
+        </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Mobile from "./Mobile.vue"
+import VueTerminal from 'vue-terminal';
+export default {
+    components: {
+        Mobile,
+        VueTerminal
+    },
+    data(){
+        return {
+            taskList:{},
+            commandList: {}
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ portfolio/src/components/Mobile.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-container class="justify-content-end" >
+  <div class="center">
+  <div class="mobile">
+    <div class="screen active"></div>
+    <div class="home"></div>
+    <div class="inner"></div>
+  </div>
+</div>
+</v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+/* body
+{
+  margin: 0;
+  padding: 0;
+} */
+/* .center
+{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+} */
+.mobile
+{
+  width: 276px;
+  height: 555px;
+  background: #fff;
+  border-radius: 30px;
+  border: 2px solid rgba(0,0,0,.3);
+  box-sizing: border-box;
+  box-shadow: inset 0 0 8px rgba(0,0,0,.3);
+}
+.screen
+{
+  background: #262626;
+  width: 256px;
+  height: 430px;
+  margin: 58px auto;
+  overflow: hidden;
+}
+.screen
+{
+  background-size: cover;
+  background-color:hsla(50, 33%, 25%, 0.75);
+
+}
+.home
+{
+  transform: translate(112px,-45px);
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #fff;
+  border: 2px solid #cecece;
+  box-sizing: border-box;
+  cursor: pointer;
+  box-shadow: inset 0 0 7px rgba(0,0,0,.1);
+}
+.inner
+{
+  width: 40px;
+  height: 4px;
+  border-radius: 2px;
+  background: #262626;
+  box-sizing: border-box;
+}
+.inner:before
+{
+  content: '';
+  background: #262626;
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+}
+.inner:after
+{
+  content: '';
+  background: #262626;
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+}
+</style>
